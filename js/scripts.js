97819fbda4268887de159bde7c2e1d804d69a995
Emily Dokken <dokkenemily@gmail.com>
2017-04-01 14:34:25 -0700

    change append to text within scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -5,35 +5,34 @@ function Pizza(size, toppings) {
   this.price = 0;
 }
 
-var topping = 1;
 var small = 12;
 var large = 24;
 
 Pizza.prototype.order = function() {
 
   if (this.size === "a" && this.toppings.length === 1) {
-    return this.price + small + topping;
+    return this.price + small + 1;
   }
    else if (this.size === "a" && this.toppings.length === 2) {
-    return this.price + small + (topping * 2);
+    return this.price + small + 2;
   }
   else if (this.size === "a" && this.toppings.length === 3) {
-   return this.price + small + (topping * 3);
+   return this.price + small + 3;
  }
   else if (this.size === "a" && this.toppings.length === 4) {
-   return this.price + small + (topping * 4);
+   return this.price + small + 4;
   }
   else if (this.size === "b" && this.toppings.length === 1) {
-   return this.price + large + topping;
+   return this.price + large + 1;
   }
   else if (this.size === "b" && this.toppings.length === 2) {
-   return this.price + large + (topping * 2);
+   return this.price + large + 2;
   }
   else if (this.size === "b" && this.toppings.length === 3) {
-   return this.price + large + (topping * 3);
+   return this.price + large + 3;
  }
   else if (this.size === "b" && this.toppings.length === 4) {
-   return this.price + large + (topping * 4);
+   return this.price + large + 4;
   }
   else if (this.size === "a") {
    return this.price += small;
@@ -59,7 +58,7 @@ $(document).ready(function() {
 
     var newPizza = new Pizza(pizzaSize, pizzaToppings);
 
-    $('#result').append(newPizza.order());
+    $('#result').text(newPizza.order());
     $('.result').show();
   });
 });
